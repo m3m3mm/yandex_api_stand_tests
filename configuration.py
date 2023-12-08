bdec246737646673b6f119b297f27a2f27b0091e
m3m3mm <153277267+m3m3mm@users.noreply.github.com>
2023-12-08 11:14:34 +0100

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://0e68e72c-6cbc-4115-8f22-3d661d8cd4e8.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
